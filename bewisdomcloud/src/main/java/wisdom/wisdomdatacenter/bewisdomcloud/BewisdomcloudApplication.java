0b15342b0b6bad636482b192b5ac35cf2ec836ff
dangvanphucnghia <nghia812014@gmail.com>
2025-10-05 12:43:01 +0700

    commit first by Nghia

---
+++ bewisdomcloud/src/main/java/wisdom/wisdomdatacenter/bewisdomcloud/BewisdomcloudApplication.java
@@ -0,0 +1,13 @@
+package wisdom.wisdomdatacenter.bewisdomcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BewisdomcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BewisdomcloudApplication.class, args);
+	}
+
+}
---
+++ bewisdomcloud/src/test/java/wisdom/wisdomdatacenter/bewisdomcloud/BewisdomcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package wisdom.wisdomdatacenter.bewisdomcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BewisdomcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
