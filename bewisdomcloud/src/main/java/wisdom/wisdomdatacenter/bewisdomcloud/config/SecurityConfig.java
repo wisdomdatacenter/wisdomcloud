e45dd0076933bec1bc269870fbf0095b3b5ba5f1
minkhoaa <tukhoa040505@gmail.com>
2025-10-07 08:54:52 +0700

    init branch khoa

--- bewisdomcloud/src/main/java/wisdom/wisdomdatacenter/bewisdomcloud/config/SecurityConfig.java
+++ bewisdomcloud/src/main/java/wisdom/wisdomdatacenter/bewisdomcloud/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,7 +18,7 @@ public class SecurityConfig {
     // Cho phép gọi /auth/** không cần auth; các API khác có thể bảo vệ sau
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated()
